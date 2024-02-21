8d7c564b55aa2733013eeb94a3a7aba72b1bf939
Vladimir Strugov <Strugov2001@yandex.ru>
2024-02-21 11:02:38 +0300

    fix: errors with links in the crumbs component

--- src/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,18 +1,23 @@
 'use client'
 
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { usePathname } from 'next/navigation'
 
 export default function BreadCrumbs () {
+  const router = useRouter()
   const location = usePathname()
   const pages = location.split(['/'])
 
   return (
     <ul className='flex gap-3 p-5 text-white drop-shadow-lg text-l'>
       <li>
-        <Link href={'/'} className='relative opacity-70 hover:opacity-100'>
+        <button
+          type='button'
+          onClick={() => router.push('/')}
+          className='relative opacity-70 hover:opacity-100'
+        >
           Домашнаяя страница
-        </Link>
+        </button>
       </li>
       {pages.map(page => {
         if (page === '') {
@@ -21,24 +26,26 @@ export default function BreadCrumbs () {
           return (
             <li key={Math.random()} className='flex gap-3'>
               <p className='opacity-70'> &gt; </p>
-              <Link
-                href={`/${page}`}
+              <button
+                type='button'
+                onClick={() => router.push(`/${page}`)}
                 className='relative opacity-70 hover:opacity-100 '
               >
                 Страница поиска
-              </Link>
+              </button>
             </li>
           )
         } else {
           return (
             <li key={Math.random()} className='flex gap-3'>
               <p className='opacity-70'> &gt; </p>
-              <Link
-                href={`/search/${page}`}
+              <button
+                type='button'
+                onClick={() => router.push(`/search/${page}`)}
                 className='relative opacity-70 hover:opacity-100 '
               >
                 {decodeURI(page)}
-              </Link>
+              </button>
             </li>
           )
         }
