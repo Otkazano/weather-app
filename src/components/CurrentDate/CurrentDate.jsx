1d1a26210fa8b492f674a523d3a15503ae32b649
Vladimir Strugov <Strugov2001@yandex.ru>
2024-02-27 15:50:42 +0300

    feat: add the current date

--- src/components/CurrentDate/CurrentDate.jsx
+++ src/components/CurrentDate/CurrentDate.jsx
@@ -4,24 +4,39 @@ import { getCurrentTime } from '@/services/getCurrentTime'
 import React from 'react'
 
 export default function CurrentDate ({ data }) {
-  const [date, setDate] = React.useState()
+  const [date, setDate] = React.useState([])
+  const [time, setTime] = React.useState([])
+  const [dayOfWeek, setDayOfWeek] = React.useState('')
+
   React.useEffect(() => {
     if (data.timezone) {
       getCurrentTime(data.timezone)
         .then(res => {
-          setDate(res)
+          const dateInfo = res.date_time_wti.split([' '])
+          console.log(dateInfo)
+          setTime(dateInfo[4].split([':']))
+          setDayOfWeek(res.date_time_txt.split([' '])[0].replace(',', ''))
+          setDate([dateInfo[1], dateInfo[2], dateInfo[3]])
         })
         .catch(err => {
           console.log(err)
         })
     }
-    console.log(date)
   }, [data])
 
   return (
     <>
-      <p>Privet</p>
-      <p>Poka</p>
+      <p>{dayOfWeek}</p>
+      <p>
+        {time[0]}:{time[1]}
+      </p>
+      {date[2] ? (
+        <p>
+          {date[0]}th {date[1]} ‘{date[2].slice(2)}
+        </p>
+      ) : (
+        ''
+      )}
     </>
   )
 }
--- src/services/getCurrentTime.js
+++ src/services/getCurrentTime.js
@@ -11,10 +11,21 @@ import axios from 'axios'
 //   }
 // }
 
+// export async function getCurrentTime (timeZone) {
+//   try {
+//     const response = await axios.get(
+//       `https://timeapi.io/api/Time/current/zone?timeZone=${timeZone}`
+//     )
+//     return response.data
+//   } catch (error) {
+//     throw new Error(error)
+//   }
+// }
+
 export async function getCurrentTime (timeZone) {
   try {
     const response = await axios.get(
-      `https://timeapi.io/api/Time/current/zone?timeZone=${timeZone}`
+      `https://api.ipgeolocation.io/timezone?apiKey=3529b8ee922d4eba911e6a385e63c993&tz=${timeZone}`
     )
     return response.data
   } catch (error) {
