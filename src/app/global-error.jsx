1056af33be01b9f8fa1531be91d9fa4e8dbb15d0
Vladimir Strugov <Strugov2001@yandex.ru>
2024-03-01 10:44:00 +0300

    refactor

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -5,19 +5,28 @@ export default function GlobalError ({}) {
   const router = useRouter()
 
   return (
-    <main className='flex flex-col grow text-white mx-auto justify-center gap-6 w-full'>
-      <h1 className='drop-shadow-lg text-9xl text-center font-semibold'>500</h1>
-      <h2 className='drop-shadow-lg text-4xl text-center font-semibold'>
-        Ошибка на стороне сервера
-      </h2>
-      <button
-        type='button'
-        onClick={() => router.back()}
-        className='w-[500px] mx-auto h-20 rounded-lg text-xl flex justify-center
-         items-center text-white tracking-widest bg-[#7e609c] drop-shadow-lg shadow-2xl opacity-90 relative hover:opacity-100 active:top-[5px]'
+    <html lang='ru'>
+      <body
+        className={`bg-main-color flex flex-col min-h-dvh overflow-x-hidden ${inter.className}`}
       >
-        Вернуться на предыдущую страницу
-      </button>
-    </main>
+        <main className='flex flex-col grow text-white mx-auto justify-center gap-6 w-full'>
+          <h1 className='drop-shadow-lg text-9xl text-center font-semibold'>
+            500
+          </h1>
+          <h2 className='drop-shadow-lg text-4xl text-center font-semibold'>
+            Ошибка на стороне сервера
+          </h2>
+          <button
+            type='button'
+            onClick={() => router.back()}
+            className='w-[500px] mx-auto h-20 rounded-lg text-xl flex justify-center
+         items-center text-white tracking-widest bg-[#7e609c] drop-shadow-lg shadow-2xl opacity-90 relative hover:opacity-100 active:top-[5px]'
+          >
+            Вернуться на предыдущую страницу
+          </button>
+        </main>
+        <Footer />
+      </body>
+    </html>
   )
 }
