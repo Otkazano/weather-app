0b5709233063f62e320726e35a511b64b2d3228e
Vladimir Strugov <Strugov2001@yandex.ru>
2024-03-01 10:53:02 +0300

    try to imtate an error

--- src/components/CurrentLocation/CurrentLocation.jsx
+++ src/components/CurrentLocation/CurrentLocation.jsx
@@ -24,7 +24,7 @@ export default function CurrentLocation () {
     getCurrentLocation(ipAddress.ip)
       .then(res => {
         if (res.location !== null) {
-          setCurrentCity(res.location)
+          setCurrentCity(res.location.asda)
         } else {
           setCurrentCity(ipAddress.city)
         }
