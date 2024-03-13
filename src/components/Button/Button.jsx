61cff33846a2e527b9b3aba73236421b7b1d0a37
Vladimir Strugov <Strugov2001@yandex.ru>
2024-03-13 13:00:21 +0300

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,6 +4,7 @@ export default function Button ({ link, text }) {
   return (
     <div className='max-w-[1020px] w-full flex justify-center m-16 mx-auto p-12 z-10 md:my-5 sm:my-2'>
       <Link
+        title='Начать поиск'
         href={link}
         className='w-full h-20 rounded-lg text-xl flex justify-center
          items-center text-white tracking-widest bg-[#7e609c] drop-shadow-lg shadow-2xl opacity-90 relative hover:opacity-100 active:top-[5px] sm:text-lg'
